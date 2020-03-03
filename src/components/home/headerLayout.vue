2d15c327467b8f3abec44916b14c9a2e0c225988
unknown <1419528576@qq.com>
2020-03-03 18:33:45 +0800

    头部信息查询

--- src/components/home/headerLayout.vue
+++ src/components/home/headerLayout.vue
@@ -6,9 +6,9 @@
         </el-col>
         <el-col :span="12">
             <el-row type="flex" justify="end" align="middle" style="padding-right:35px">
-                <img src="https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3256100974,305075936&fm=26&gp=0.jpg" alt="" style="width:35px;height:35px;border-radius:50%;margin-right:10px">
+                <img :src="userInfo.photo" alt="" style="width:35px;height:35px;border-radius:50%;margin-right:10px">
                 <el-dropdown trigger="click">
-                    <span>宁i<i class="el-icon-caret-bottom"></i></span>
+                    <span>{{userInfo.name}}<i class="el-icon-caret-bottom"></i></span>
                       <el-dropdown-menu slot="dropdown">
                       <el-dropdown-item>个人信息</el-dropdown-item>
                       <el-dropdown-item>git地址</el-dropdown-item>
@@ -22,7 +22,26 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+  // 创建完实例执行
+  created () {
+    // 获取token 登录进来的时候回存在一个钥匙 token
+    const TOKEN = window.localStorage.getItem('user_token')
+    // console.log(TOKEN)
+    this.$axios({ // 这个请求的时候需要请求头  这里的请求方式是get  可以省略
+      url: '/user/profile',
+      headers: {
+        Authorization: `Bearer ${TOKEN}`
+      }
+    }).then(res => {
+    //   console.log(res)
+      this.userInfo = res.data.data
+    })
+  }
 }
 </script>
 
