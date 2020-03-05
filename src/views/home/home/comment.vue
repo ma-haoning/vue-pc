765816ac567ae10ff1039f239c281a6169ea3078
unknown <1419528576@qq.com>
2020-03-05 11:22:57 +0800

    评论列表表格添加背景颜色和鼠标悬浮时当前行变化背景颜色

--- src/views/home/home/comment.vue
+++ src/views/home/home/comment.vue
@@ -6,7 +6,7 @@
         <template slot="breadCrumb">评论列表</template>
     </breadCrumb>
     <!-- 第二个是表格的内容  用data属性来获取数据  再通过prop属性把字段名字写在上面内部会自己运行-->
-    <el-table :data="list">
+    <el-table :data="list" :row-class-name="rowShowColor">
         <el-table-column label="标题" width="600" prop="id"></el-table-column>
         <el-table-column label="评论状态" prop="status" align="center"></el-table-column>
         <el-table-column label="总评论数" prop="zong" align="center"></el-table-column>
@@ -29,10 +29,33 @@ export default {
         { id: 4, status: '正常', zong: 200, fans: 20 },
         { id: 5, status: '正常', zong: 200, fans: 20 }]
     }
+  },
+  methods: {
+    // 每一行的背景颜色
+    rowShowColor ({ row, rowIndex }) {
+      // console.log(row) // 显示的是每一行的内容 是一个对象
+      // console.log(rowIndex) // 显示的是行的索引
+      if (rowIndex + 1) {
+        return 'color'
+      } else {
+        return ''
+      }
+    }
   }
 }
 </script>
 
 <style>
+/* 初步判断 elementui不承认less的用法 */
+ .el-table .color {
+    background: oldlace;
+ }
+/* 鼠标悬浮在当前的行上 name当前行的所有列的背景变颜色 */
+ .el-table tr:hover>td{
+    background-color: #c6cfdf !important;
+}
 
+ /* .el-table tr:hover>td{
+   background-color:red !important;
+ } */
 </style>
