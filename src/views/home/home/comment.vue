49c50fdbd3d2a89352255eab55e885ad247dd190
unknown <1419528576@qq.com>
2020-03-06 13:54:12 +0800

    分页布局请求和加载

--- src/views/home/home/comment.vue
+++ src/views/home/home/comment.vue
@@ -6,7 +6,7 @@
         <template slot="breadCrumb">评论列表</template>
     </breadCrumb>
     <!-- 第二个是表格的内容  用data属性来获取数据  再通过prop属性把字段名字写在上面内部会自己运行-->
-    <el-table :data="list" :row-class-name="rowShowColor">
+    <el-table :data="list" :row-class-name="rowShowColor" v-loading="loading">
         <el-table-column label="标题" width="600" prop="title"></el-table-column>
         <el-table-column label="评论状态" prop="comment_status" align="center" :formatter="formatterShow"></el-table-column>
         <el-table-column label="总评论数" prop="total_comment_count" align="center"></el-table-column>
@@ -19,6 +19,10 @@
            </template>
         </el-table-column>
     </el-table>
+    <!-- 添加分页的功能 -->
+    <el-row type="flex" style="height:80px" align="middle" justify="center">
+      <el-pagination layout="prev,pager,next" :total="paginations.total" background :page-size="paginations.much_page" :current-page="paginations.current_page"  @current-change="currentChange"></el-pagination>
+    </el-row>
     </el-card>
 </template>
 
@@ -26,7 +30,15 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      paginations: {
+        // 分页的重要成员  这里的成员的数量可以随机定义 因为获取到请求数据之后把总数和每页的条数都会反馈出一个新数据
+        current_page: 1,
+        total: 1000,
+        much_page: 1
+      },
+      // 加载转圈圈
+      loading: true
     }
   },
   methods: {
@@ -42,16 +54,26 @@ export default {
     },
     // 发送请求 接收数据
     getComment () {
+      // 每次发送请求之前loading
+      this.loading = true
       this.$axios({
         url: '/articles',
         params: {
-          response_type: 'comment' // 因为这个接口对应好几个不同参数的数据
+          response_type: 'comment', // 因为这个接口对应好几个不同参数的数据
+          page: this.paginations.current_page // 这是发送请求时传的参数 当前页码的参数
+          // pre_page: this.paginations.much_page // 这是每页的条数
         }
       }).then((res) => {
         // 这里是箭头函数 可以随便用this
         console.log(res)
+        // res返回的数据有要显示的分页总数数据
+        this.paginations.total = res.data.total_count
+        // res返回的数据有要显示的每页的条数的多少
+        this.paginations.much_page = res.data.per_page
         // 因为el-table不能处理boolean值 所以需要
         this.list = res.data.results
+        // 这里请求结束再让loading==false
+        this.loading = false
       })
     },
     // 格式化 true和false的那一列
@@ -100,6 +122,12 @@ export default {
             center: true
           })
         })
+    },
+    // 切换页码会执行该函数
+    currentChange (newPage) {
+      // alert(newPage) // 这里的newPage就可以获取到要去的位置页码
+      this.paginations.current_page = newPage // 把当前的点击的页码给了发送请求的参数会重新赋值
+      this.getComment() // 再次调用有新参数的获取评论列表的方法
     }
   },
   // 实例创建完执行此函数
