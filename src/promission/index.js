fe140d1b4302a88581de3424fe94b95593c0a53a
unknown <1419528576@qq.com>
2020-03-10 18:52:50 +0800

    路由进度条显示和关闭

--- src/promission/index.js
+++ src/promission/index.js
@@ -1,10 +1,14 @@
 // 专门处理路由权限
 // 引入 路由
 import router from '@/router'
+import progress from 'nprogress' // 进度条包
+import 'nprogress/nprogress.css' // 样式文件
 router.beforeEach(function (to, from, next) {
-//   console.log(to)
-//   next()
-//   console.log(to)
+  // 使用进度条
+  progress.start()
+  //   console.log(to)
+  //   next()
+  //   console.log(to)
   // to 代表到哪里去  from 代表从哪里来 next 不管去哪里都要执行一下
   // 判断如果有home需要拦截  没有home就只有登录不需要拦截 要不然没办法登录
   if (to.path.startsWith('/home')) {
@@ -18,4 +22,9 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(() => {
+  // 结束进度条  设置0.2秒之后结束进度条
+  setTimeout(() => progress.done(), 200)
+  // progress.done()
 })
